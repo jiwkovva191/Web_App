eea8b2cb8bcad17f97f2c45be16403827e017a8f
jiwkovva19 <yankowadelyana@gmail.com>
2026-03-14 20:23:38 +0200

    Added using of userRoutes

--- index.ts
+++ index.ts
@@ -1,10 +1,13 @@
 import express, { Application, json, NextFunction} from "express";
+import userRoutes from "./routes/user.routes";
 
 const app: Application= express();
 const port = 3000;
 
 
 app.use(json());
+
+app.use(userRoutes)
 
 app.listen(port, ()=>{
    console.log(`Listening on port ${port}`) 
