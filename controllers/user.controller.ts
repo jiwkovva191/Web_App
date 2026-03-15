234f945ce7f9e1ac09606fe32a658aa3dccd6791
jiwkovva19 <yankowadelyana@gmail.com>
2026-03-15 07:31:55 +0200

    Added CRUD logic to the controller

--- controllers/user.controller.ts
+++ controllers/user.controller.ts
@@ -1,35 +1,68 @@
 import {Request, Response} from "express";
+import { UserService } from "../services/user.service";
 
 export class UserController{
+
+    constructor(private userService: UserService){
+
+    }
+
     getAll = (req: Request, res: Response)=>{
+
+        const users = this.userService.getAll()
+
         res.json({
-            'message':'Users',
+            'users' : users
         })
     }
 
     findById = (req: Request, res: Response)=>{
         
+        const { id } = req.params
+        const user = this.userService.findById(id as string)
+        if(!user){
+            res.json({
+                'message' : 'User not found'
+            })
+            return
+        }
         res.json({
-            'message':'User'
+            'user':user
         })
     }
     
     create = (req: Request, res: Response)=>{
-        
+        const { name, email } = req.body
+        const user = this.userService.create({ name, email })
         res.json({
-            'message':'User created'
+            'user': user
         })
     }
 
     update = (req: Request, res: Response)=>{
-        
+        const { id } = req.params
+        const data = req.body
+        const user = this.userService.update(id as string, data)
+
+        if(!user){
+            res.json({
+                'message' : 'User not found'
+            })
+            return
+        }
         res.json({
-            'message':'User updated'
+            'user': user
         })
     }
 
     delete = (req: Request, res: Response)=>{
-        
+        const { id } = req.params
+        const deleted = this.userService.delete(id as string)
+        if(!deleted){
+            res.json({
+                'message':'User not found'
+            })
+        }
         res.json({
             'message':'User deleted'
         })
