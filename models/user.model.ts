036e440fe57eca3d74c13e831849ede52936981d
jiwkovva19 <yankowadelyana@gmail.com>
2026-03-15 07:44:54 +0200

    Added user.model.ts file

---
+++ models/user.model.ts
@@ -0,0 +1,38 @@
+import { User } from "../types/User";
+
+export class UserModel{
+    // TODO: replace with db when available
+    constructor(private users: Map<string, User>){
+        
+    }
+
+    findAll(): User[]{
+        return [...this.users.values()]
+    }
+
+    findById(id: string): User | undefined{
+        return this.users.get(id)
+    }
+
+    create(user: User){
+        this.users.set(user.id, user)
+        return user
+    }
+
+    update(id: string, data: Partial<User>): User | undefined{
+        const existingUser = this.users.get(id)
+        if(!existingUser){
+            return undefined
+        }
+
+        // TODO: ???
+        const updatedUser = {...existingUser, ...data, id}
+        this.users.set(id, updatedUser)
+        return updatedUser
+
+    }
+
+    delete(id: string): boolean{
+        return this.users.delete(id)
+    }
+}
