c01410fe0e46253163335a2f40e83ff7aa369013
jiwkovva19 <yankowadelyana@gmail.com>
2026-03-22 07:59:51 +0200

    made some of the methods async

--- services/user.service.ts
+++ services/user.service.ts
@@ -1,32 +1,28 @@
 import { UserModel } from "../models/user.model";
-import { User } from "../types/User";
+import { CreateUserDTO, User } from "../types/User";
 
 export class UserService {
 
-        constructor(private userModel: UserModel){
+        constructor(private userModel: UserModel){}
 
-        }
-
-        getAllUsers(): User[] {
-            return this.userModel.findAll();
+        async getAllUsers(): Promise<User[]> {
+            return await this.userModel.findAll();
         }
         
     
-        getUserById(id: string): User | undefined {
-            return this.userModel.findById(id)
+        async getUserById(id: number): Promise<User | undefined>{
+            return await this.userModel.findById(id)
         }
         
-        //data holds the data for the user creation
-        createUser(data: { name: string, email: string}): User {
-            const id = Date.now().toString()
-            const user: User = {id, ...data}
-            return this.userModel.create(user)
+        async createUser(data: CreateUserDTO): Promise<User> {
+            return await this.userModel.create(data)
         }
-        updateUser(id: string, data: Partial<User>): User | undefined {
+
+        updateUser(id: number, data: Partial<User>): User | undefined {
             return this.userModel.update(id, data)
         }
     
-        deleteUser(id:string): boolean {
+        deleteUser(id:number): boolean {
             return this.userModel.delete(id)
         }
 }
