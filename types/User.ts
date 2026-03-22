ca01a55ddf330c7fa8f65bb9167952338eac8f64
jiwkovva19 <yankowadelyana@gmail.com>
2026-03-22 07:55:17 +0200

    added new interface CreateUserDTO

--- types/User.ts
+++ types/User.ts
@@ -1,5 +1,11 @@
 export interface User {
-    id: string;
+    id: number;
     name: string;
+    email: string
+}
+
+/*DTO - Data Transfer Object*/
+export interface CreateUserDTO{
+    name: string,
     email: string
 }
