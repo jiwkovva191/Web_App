e88b6afe229dff4bd8fb982e9986f273e026d2f3
jiwkovva19 <yankowadelyana@gmail.com>
2026-03-22 08:05:05 +0200

    made async methods

--- controllers/user.controller.ts
+++ controllers/user.controller.ts
@@ -9,9 +9,9 @@ export class UserController{
 
     constructor(private userService: UserService){}
 
-    getAll = (req: Request, res: Response): void=>{
+    getAll = async (req: Request, res: Response): Promise<void>=>{
 
-        const users = this.userService.getAllUsers()
+        const users = await this.userService.getAllUsers()
 
         res.json({
             message : 'User found',
@@ -19,9 +19,10 @@ export class UserController{
         })
     }
 
-    getById = (req: Request<IdParams>, res: Response): void=>{
+    getById = async (req: Request<IdParams>, res: Response): Promise<void>=>{
         
-        const user = this.userService.getUserById(req.params.id)
+        const id = Number(req.params.id)
+        const user = await this.userService.getUserById(id)
         if(!user){
             res.status(404).json({
                 'message' : 'User not found'
@@ -34,7 +35,7 @@ export class UserController{
         })
     }
     
-    create = (req: Request, res: Response): void=>{
+    create = async (req: Request, res: Response): Promise<void>=>{
         const { name, email } = req.body
         const user = this.userService.createUser({ name, email })
         res.status(201).json({
@@ -44,7 +45,8 @@ export class UserController{
     }
 
     update = (req: Request<IdParams>, res: Response): void=>{
-        const user = this.userService.updateUser(req.params.id, req.body)
+        const id = Number(req.params.id)
+        const user = this.userService.updateUser(id, req.body)
 
         if(!user){
             res.status(404).json({
@@ -59,7 +61,8 @@ export class UserController{
     }
 
     delete = (req: Request<IdParams>, res: Response): void=>{
-        const deleted = this.userService.deleteUser(req.params.id)
+        const id = Number(req.params.id)
+        const deleted = this.userService.deleteUser(id)
         if(!deleted){
             res.status(404).json({
                 'message':'User not found'
