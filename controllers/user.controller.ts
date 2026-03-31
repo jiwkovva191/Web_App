9cd947f0b46abbf4aebded77b50374e5861c1bf4
jiwkovva19 <yankowadelyana@gmail.com>
2026-03-31 17:02:37 +0300

    changes

--- controllers/user.controller.ts
+++ controllers/user.controller.ts
@@ -2,7 +2,8 @@ import {Request, Response} from "express";
 import { UserService } from "../services/user.service";
 
 interface IdParams {
-    id: string
+    id: string,
+    email: string
 }
 
 export class UserController{
@@ -34,19 +35,34 @@ export class UserController{
             data :user
         })
     }
+
+    getByEmail = async (req: Request<IdParams>, res: Response): Promise<void>=>{
+        
+        const user = await this.userService.getUserByEmail(req.params.email)
+        if(!user){
+            res.status(404).json({
+                'message' : 'User not found'
+            })
+            return
+        }
+        res.json({
+            message : 'User found',
+            data :user
+        })
+    }
     
     create = async (req: Request, res: Response): Promise<void>=>{
         const { name, email } = req.body
-        const user = this.userService.createUser({ name, email })
+        const user = await this.userService.createUser({ name, email })
         res.status(201).json({
             message : 'User created',
             data : user
         })
     }
 
-    update = (req: Request<IdParams>, res: Response): void=>{
+    update = async (req: Request<IdParams>, res: Response): Promise<void>=>{
         const id = Number(req.params.id)
-        const user = this.userService.updateUser(id, req.body)
+        const user = await this.userService.updateUser(id, req.body)
 
         if(!user){
             res.status(404).json({
@@ -60,9 +76,9 @@ export class UserController{
         })
     }
 
-    delete = (req: Request<IdParams>, res: Response): void=>{
+    delete = async (req: Request<IdParams>, res: Response): Promise<void>=>{
         const id = Number(req.params.id)
-        const deleted = this.userService.deleteUser(id)
+        const deleted = await this.userService.deleteUser(id)
         if(!deleted){
             res.status(404).json({
                 'message':'User not found'
